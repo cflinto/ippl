03f0b0acf94d6322feedfcf24596f2a4cff77353
cflint <clement.flint@tum.de>
2026-06-03 15:36:46 +0200

    old change, probably meaningless

--- src/PoissonSolvers/IpplLaplaceOp.h
+++ src/PoissonSolvers/IpplLaplaceOp.h
@@ -17,12 +17,16 @@ public:
         : gko::EnableLinOp<IpplLaplaceOp<FieldType>>(exec) {}
 
     IpplLaplaceOp(std::shared_ptr<const gko::Executor> exec, gko::dim<2> size,
-                  FieldType& templated_field, int nx, int ny, int nz, int nghost)
-        : gko::EnableLinOp<IpplLaplaceOp<FieldType>>(exec, size),
-          nx_(nx), ny_(ny), nz_(nz), nghost_(nghost)
+                  FieldType& templated_field)
+        : gko::EnableLinOp<IpplLaplaceOp<FieldType>>(exec, size)
     {
-        temp_in_ = std::make_shared<FieldType>(templated_field.get_mesh(), templated_field.getLayout());
-        temp_out_ = std::make_shared<FieldType>(templated_field.get_mesh(), templated_field.getLayout());
+        // Fix the Assertion 'layout_m != 0' failed by initializing properly!
+        temp_in_ = std::make_shared<FieldType>();
+        temp_in_->initialize(templated_field.get_mesh(), templated_field.getLayout());
+        
+        temp_out_ = std::make_shared<FieldType>();
+        temp_out_->initialize(templated_field.get_mesh(), templated_field.getLayout());
+        
         temp_in_->setFieldBC(templated_field.getFieldBC());
         temp_out_->setFieldBC(templated_field.getFieldBC());
     }
@@ -32,17 +36,30 @@ protected:
         static IpplTimings::TimerRef packTimer = IpplTimings::getTimer("Ginkgo MF: Pack/Unpack");
         static IpplTimings::TimerRef mathTimer = IpplTimings::getTimer("Ginkgo MF: Laplace Math");
 
-        auto dense_in = gko::as<gko::matrix::Dense<double>>(in);
-        auto dense_out = gko::as<gko::matrix::Dense<double>>(out);
+        // The vectors passed by Ginkgo's distributed CG are distributed vectors!
+        auto dist_in = gko::as<gko::experimental::distributed::Vector<double>>(in);
+        auto dist_out = gko::as<gko::experimental::distributed::Vector<double>>(out);
+        
+        // Get the local dense pieces of those vectors
+        auto dense_in = dist_in->get_local_vector();
+        auto dense_out = dist_out->get_local_vector();
+
         auto view_in = temp_in_->getView();
         const double* gko_in_data = dense_in->get_const_values();
-        int nghost = nghost_; int ny = ny_; int nz = nz_;
+        
+        int nghost = temp_in_->getNghost();
+        auto& fl = temp_in_->getLayout();
+        const auto& local_domain = fl.getLocalNDIndex();
+        int nx_local = local_domain[0].length();
+        int ny_local = local_domain[1].length();
+        int nz_local = local_domain[2].length();
         
         IpplTimings::startTimer(packTimer);
-        Kokkos::parallel_for("CopyIn", temp_in_->getFieldRangePolicy(),
+        Kokkos::parallel_for("CopyIn", 
+            Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {nx_local, ny_local, nz_local}),
             KOKKOS_LAMBDA(const int i, const int j, const int k) {
-                int row = (i - nghost) * ny * nz + (j - nghost) * nz + (k - nghost);
-                view_in(i, j, k) = gko_in_data[row];
+                int row = i * ny_local * nz_local + j * nz_local + k;
+                view_in(i + nghost, j + nghost, k + nghost) = gko_in_data[row];
             });
         Kokkos::fence();
         IpplTimings::stopTimer(packTimer);
@@ -55,10 +72,11 @@ protected:
         IpplTimings::startTimer(packTimer);
         auto view_out = temp_out_->getView();
         double* gko_out_data = dense_out->get_values();
-        Kokkos::parallel_for("CopyOut", temp_out_->getFieldRangePolicy(),
+        Kokkos::parallel_for("CopyOut", 
+            Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {nx_local, ny_local, nz_local}),
             KOKKOS_LAMBDA(const int i, const int j, const int k) {
-                int row = (i - nghost) * ny * nz + (j - nghost) * nz + (k - nghost);
-                gko_out_data[row] = view_out(i, j, k);
+                int row = i * ny_local * nz_local + j * nz_local + k;
+                gko_out_data[row] = view_out(i + nghost, j + nghost, k + nghost);
             });
         Kokkos::fence();
         IpplTimings::stopTimer(packTimer);
@@ -66,24 +84,26 @@ protected:
 
     void apply_impl(const gko::LinOp* alpha, const gko::LinOp* in,
                     const gko::LinOp* beta, gko::LinOp* out) const override {
-        auto dense_out = gko::as<gko::matrix::Dense<double>>(out);
+        auto dist_out = gko::as<gko::experimental::distributed::Vector<double>>(out);
         auto dense_alpha = gko::as<gko::matrix::Dense<double>>(alpha);
         auto dense_beta = gko::as<gko::matrix::Dense<double>>(beta);
 
         if (!temp_math_result_) {
-            temp_math_result_ = gko::share(gko::matrix::Dense<double>::create(this->get_executor(), dense_out->get_size()));
+            // Clone the vector to guarantee exact MPI distributed sizing
+            temp_math_result_ = std::dynamic_pointer_cast<gko::experimental::distributed::Vector<double>>(
+                gko::share(dist_out->clone())
+            );
         }
         
         this->apply_impl(in, temp_math_result_.get());
-        dense_out->scale(dense_beta);
-        dense_out->add_scaled(dense_alpha, temp_math_result_.get());
+        dist_out->scale(dense_beta);
+        dist_out->add_scaled(dense_alpha, temp_math_result_.get());
     }
 
 private:
-    mutable std::shared_ptr<gko::matrix::Dense<double>> temp_math_result_;
+    mutable std::shared_ptr<gko::experimental::distributed::Vector<double>> temp_math_result_;
     mutable std::shared_ptr<FieldType> temp_in_;
     mutable std::shared_ptr<FieldType> temp_out_;
-    int nx_, ny_, nz_, nghost_;
 };
 
 } // namespace ippl
--- src/PoissonSolvers/PoissonGinkgo.h
+++ src/PoissonSolvers/PoissonGinkgo.h
@@ -6,12 +6,10 @@
 #include <ginkgo/ginkgo.hpp>
 #include <memory>
 #include "PoissonSolvers/Poisson.h"
-#include "PoissonSolvers/IpplLaplaceOp.h" 
 #include "Utility/IpplTimings.h"
 
 namespace ippl {
 
-// We now pass the Vector Field type directly so we don't have to guess it!
 template <typename FieldLHS, typename FieldRHS = FieldLHS, typename VFieldType = FieldLHS>
 class PoissonGinkgo : public Poisson<FieldLHS, FieldRHS> {
     using Tlhs = typename FieldLHS::value_type;
@@ -21,116 +19,171 @@ public:
     constexpr static unsigned Dim = FieldLHS::dim;
 
     PoissonGinkgo() {
-        this->params_m.add("solver_type", "cg");
         this->params_m.add("max_iterations", 1000);
         this->params_m.add("tolerance", (Tlhs)1e-13);
-        this->params_m.add("preconditioner", "none"); 
+        this->params_m.add("preconditioner", "jacobi"); // Can be "jacobi", "ilu", or "none"
     }
 
     void mergeParameters(const ParameterList& params) { this->params_m.merge(params); }
     void setRhs(FieldRHS& rhs) override { this->rhs_mp = &rhs; }
     void setLhs(FieldLHS& lhs) override { this->lhs_mp = &lhs; }
-    
-    // Gradient method now uses the provided VFieldType safely
     void setGradient(VFieldType& grad) { this->grad_mp = &grad; }
-    
     int getIterationCount() const { return itCount_; }
 
     void setup() {
         exec_ = gko::OmpExecutor::create();
         
         auto& mesh = this->rhs_mp->get_mesh();
-        int nx = mesh.getGridsize(0);
-        int ny = mesh.getGridsize(1);
-        int nz = mesh.getGridsize(2);
-        int N = nx * ny * nz;
-
-        int nghost = this->lhs_mp->getNghost();
-
-        system_matrix_ = gko::share(IpplLaplaceOp<FieldLHS>::create(
-            exec_, gko::dim<2>{(size_t)N, (size_t)N}, *(this->lhs_mp), nx, ny, nz, nghost));
-
-        int max_iters = this->params_m.template get<int>("max_iterations");
-        double tol = this->params_m.template get<Tlhs>("tolerance");
-        std::string solver_type = this->params_m.template get<std::string>("solver_type");
-
-        auto stop_criterion = gko::stop::Iteration::build().with_max_iters(max_iters).on(exec_);
-        auto res_criterion = gko::stop::ResidualNorm<Tlhs>::build().with_reduction_factor(tol).on(exec_);
-
-        auto shared_stop = gko::share(std::move(stop_criterion));
-        auto shared_res = gko::share(std::move(res_criterion));
-
-        if (solver_type == "cg") {
-            auto builder = gko::solver::Cg<Tlhs>::build().with_criteria(shared_stop, shared_res);
-            solver_ = builder.on(exec_)->generate(system_matrix_);
-        } else {
-            auto builder = gko::solver::Gmres<Tlhs>::build().with_criteria(shared_stop, shared_res);
-            solver_ = builder.on(exec_)->generate(system_matrix_);
-        }
+        nx_ = mesh.getGridsize(0); ny_ = mesh.getGridsize(1); nz_ = mesh.getGridsize(2);
         
-        logger_ = gko::log::Convergence<Tlhs>::create();
-        solver_->add_logger(logger_);
+        auto& fl = this->rhs_mp->getLayout();
+        const auto& local_domain = fl.getLocalNDIndex();
+        nx_local_ = local_domain[0].length();
+        ny_local_ = local_domain[1].length();
+        nz_local_ = local_domain[2].length();
+        local_size_ = nx_local_ * ny_local_ * nz_local_;
+
+        std::string prec_type = this->params_m.template get<std::string>("preconditioner");
+
+        if (prec_type != "none") {
+            // Generate a local sub-matrix block for preconditioning on this rank
+            gko::matrix_data<double, int> md{gko::dim<2>{(size_t)local_size_, (size_t)local_size_}};
+            double dx = mesh.getMeshSpacing(0); double dy = mesh.getMeshSpacing(1); double dz = mesh.getMeshSpacing(2);
+            double inv_dx2 = 1.0 / (dx * dx); double inv_dy2 = 1.0 / (dy * dy); double inv_dz2 = 1.0 / (dz * dz);
+            double diag = 2.0 * (inv_dx2 + inv_dy2 + inv_dz2);
+
+            // Simple local 7-point stencil setup for the preconditioner block
+            for (int i = 0; i < nx_local_; ++i) {
+                for (int j = 0; j < ny_local_; ++j) {
+                    for (int k = 0; k < nz_local_; ++k) {
+                        int row = i * ny_local_ * nz_local_ + j * nz_local_ + k;
+                        md.nonzeros.emplace_back(row, row, diag);
+                        if (i > 0) md.nonzeros.emplace_back(row, (i-1)*ny_local_*nz_local_ + j*nz_local_ + k, -inv_dx2);
+                        if (i < nx_local_-1) md.nonzeros.emplace_back(row, (i+1)*ny_local_*nz_local_ + j*nz_local_ + k, -inv_dx2);
+                        if (j > 0) md.nonzeros.emplace_back(row, i*ny_local_*nz_local_ + (j-1)*nz_local_ + k, -inv_dy2);
+                        if (j < ny_local_-1) md.nonzeros.emplace_back(row, i*ny_local_*nz_local_ + (j+1)*nz_local_ + k, -inv_dy2);
+                        if (k > 0) md.nonzeros.emplace_back(row, i*ny_local_*nz_local_ + j*nz_local_ + (k-1), -inv_dz2);
+                        if (k < nz_local_-1) md.nonzeros.emplace_back(row, i*ny_local_*nz_local_ + j*nz_local_ + (k+1), -inv_dz2);
+                    }
+                }
+            }
+            auto local_mat = gko::share(gko::matrix::Csr<double, int>::create(exec_));
+            local_mat->read(md);
+
+            if (prec_type == "jacobi") {
+                prec_factory_ = gko::share(gko::preconditioner::Jacobi<double, int>::build().on(exec_)->generate(local_mat));
+            } else if (prec_type == "ilu") {
+                using L_Solver = gko::solver::LowerTrs<double, int>;
+                using U_Solver = gko::solver::UpperTrs<double, int>;
+                prec_factory_ = gko::share(gko::preconditioner::Ilu<L_Solver, U_Solver>::build().on(exec_)->generate(local_mat));
+            }
+        }
+    }
+
+    // High-performance local preconditioning hook
+    void applyPreconditioner(FieldLHS& r, FieldLHS& z) {
+        if (!prec_factory_) {
+            z = r;
+            return;
+        }
+        auto view_r = r.getView(); auto view_z = z.getView();
+        int nghost = r.getNghost();
+
+        auto gko_r = gko::matrix::Dense<double>::create(exec_, gko::dim<2>{(size_t)local_size_, 1});
+        auto gko_z = gko::matrix::Dense<double>::create(exec_, gko::dim<2>{(size_t)local_size_, 1});
+
+        double* r_vals = gko_r->get_values(); double* z_vals = gko_z->get_values();
+        int ny = ny_local_, nz = nz_local_;
+
+        Kokkos::parallel_for("PackPrec", Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0,0,0},{nx_local_,ny_local_,nz_local_}),
+            KOKKOS_LAMBDA(const int i, const int j, const int k) {
+                r_vals[i*ny*nz + j*nz + k] = view_r(i+nghost, j+nghost, k+nghost);
+            });
+        Kokkos::fence();
+
+        prec_factory_->apply(gko_r.get(), gko_z.get());
+
+        Kokkos::parallel_for("UnpackPrec", Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0,0,0},{nx_local_,ny_local_,nz_local_}),
+            KOKKOS_LAMBDA(const int i, const int j, const int k) {
+                view_z(i+nghost, j+nghost, k+nghost) = z_vals[i*ny*nz + j*nz + k];
+            });
+        Kokkos::fence();
+    }
+    
+    // Natively distributed dot product using IPPL Comm
+    double getDotProduct(FieldLHS& a, FieldLHS& b) {
+        auto view_a = a.getView();
+        auto view_b = b.getView();
+        int nghost = a.getNghost();
+        int nx = nx_local_, ny = ny_local_, nz = nz_local_;
+
+        double local_dot = 0.0;
+        Kokkos::parallel_reduce("DotProduct", 
+            Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {nx, ny, nz}),
+            KOKKOS_LAMBDA(const int i, const int j, const int k, double& lsum) {
+                lsum += view_a(i + nghost, j + nghost, k + nghost) * view_b(i + nghost, j + nghost, k + nghost);
+            }, Kokkos::Sum<double>(local_dot));
+        Kokkos::fence();
+
+        double global_dot = 0.0;
+        ippl::Comm->reduce(local_dot, global_dot, 1, std::plus<double>());
+        return global_dot;
     }
 
     void solve() override {
-        if (!solver_) {
-            this->setup();
+        if (!exec_) this->setup();
+
+        FieldLHS& x = *(this->lhs_mp); FieldRHS& b = *(this->rhs_mp);
+        FieldLHS r, d, q, s;
+        r.initialize(b.get_mesh(), b.getLayout()); d.initialize(b.get_mesh(), b.getLayout());
+        q.initialize(b.get_mesh(), b.getLayout()); s.initialize(b.get_mesh(), b.getLayout());
+
+        int max_iters = this->params_m.template get<int>("max_iterations");
+        double tol = this->params_m.template get<Tlhs>("tolerance");
+
+        // 1. Distributed PCG Initializations
+        x.fillHalo();
+        r = b - (-laplace(x)); 
+        this->applyPreconditioner(r, d); 
+        
+        double delta_new = getDotProduct(r, d); 
+        double delta_0 = delta_new;
+        itCount_ = 0;
+
+        // 2. Globally Synchronized Solver Loop
+        while (itCount_ < max_iters && delta_new > tol * tol * delta_0) {
+            d.fillHalo(); // Crucial for distributed derivatives!
+            q = -laplace(d);
+            double alpha = delta_new / getDotProduct(d, q);
+            
+            x = x + alpha * d;
+            r = r - alpha * q;
+            
+            this->applyPreconditioner(r, s);
+            double delta_old = delta_new;
+            delta_new = getDotProduct(r, s);
+            
+            double beta = delta_new / delta_old;
+            d = s + beta * d;
+            itCount_++;
         }
 
-        int N = system_matrix_->get_size()[0];
-
-        auto view_rhs = this->rhs_mp->getView();
-        int nghost = this->rhs_mp->getNghost();
-        int ny = this->rhs_mp->get_mesh().getGridsize(1);
-        int nz = this->rhs_mp->get_mesh().getGridsize(2);
-        
-        Kokkos::View<double*> staging_b("staging_b", N);
-        Kokkos::View<double*> staging_x("staging_x", N);
-        
-        auto local_b = staging_b;
-        auto local_x = staging_x;
-        
-        Kokkos::parallel_for("PackRHS", this->rhs_mp->getFieldRangePolicy(),
-            KOKKOS_LAMBDA(const int i, const int j, const int k) {
-                int row = (i - nghost) * ny * nz + (j - nghost) * nz + (k - nghost);
-                local_b(row) = view_rhs(i, j, k);
-                local_x(row) = 0.0;
-            });
-        Kokkos::fence();
-
-        auto gko_b = gko::share(gko::matrix::Dense<Tlhs>::create(
-            exec_, gko::dim<2>{(size_t)N, 1}, gko::array<Tlhs>::view(exec_, N, staging_b.data()), 1));
-        auto gko_x = gko::share(gko::matrix::Dense<Tlhs>::create(
-            exec_, gko::dim<2>{(size_t)N, 1}, gko::array<Tlhs>::view(exec_, N, staging_x.data()), 1));
-
-        solver_->apply(gko_b.get(), gko_x.get());
-        itCount_ = logger_->get_num_iterations();
-
-        auto view_lhs = this->lhs_mp->getView();
-        Kokkos::parallel_for("UnpackLHS", this->lhs_mp->getFieldRangePolicy(),
-            KOKKOS_LAMBDA(const int i, const int j, const int k) {
-                int row = (i - nghost) * ny * nz + (j - nghost) * nz + (k - nghost);
-                view_lhs(i, j, k) = local_x(row);
-            });
-        Kokkos::fence();
-
         if (this->grad_mp) {
-            this->lhs_mp->fillHalo();
-            *(this->grad_mp) = -grad(*(this->lhs_mp));
+            x.fillHalo();
+            *(this->grad_mp) = -grad(x);
         }
     }
 
 private:
     int itCount_ = 0;
-    VFieldType* grad_mp = nullptr; 
-    
+    VFieldType* grad_mp = nullptr;
+    int nx_ = 0, ny_ = 0, nz_ = 0;
+    int nx_local_ = 0, ny_local_ = 0, nz_local_ = 0, local_size_ = 0;
+
     std::shared_ptr<gko::Executor> exec_;
-    std::shared_ptr<const gko::LinOp> system_matrix_;
-    std::unique_ptr<gko::LinOp> solver_;
-    std::shared_ptr<const gko::log::Convergence<Tlhs>> logger_;
+    std::shared_ptr<gko::LinOp> prec_factory_;
 };
 
 } // namespace ippl
-
 #endif // ENABLE_GINKGO
 #endif // IPPL_POISSON_GINKGO_H
